a5d8b19310d60800fdca7a8a07423bb5d19c59b1
hceisler <hceisler@gmail.com>
2022-09-16 20:23:41 -0400

    monthcal finished

--- Projects/pr_02/monthCal.m
+++ Projects/pr_02/monthCal.m
@@ -1,1 +1,22 @@
-%test
+% prints a one-month calendar
+days = input("Enter how many days in the month: ");
+dayofweek = input("Starting day-of-the-week (1=Mon, 7=Sun): ");
+
+
+fprintf("Su Mo Tu We Th Fr Sa\n")
+if dayofweek ~= 7
+   for j = 1:rem(dayofweek,7)
+   fprintf("   ")
+   end
+end
+for i = 1:days
+    if rem(i+(7-dayofweek),7) == 0
+        fprintf("\n")
+    end
+    if i<10
+        fprintf("%d  ",i)
+    else
+        fprintf("%d ",i)
+    end
+end
+fprintf("\n")
