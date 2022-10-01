1b8fb023836ac50060efdbebf561c3f8977c0f8b
GoldenSpamfish <ryanellis3@gmail.com>
2022-10-01 00:15:54 -0400

    finished part 2

---
+++ Projects/pr_03/golfTrajSensitivity.m
@@ -0,0 +1,61 @@
+close all % Close all figure windows
+figure % Start a new figure
+set(gcf,'Units','normalized','Position',[.3 .2 .5 .6])
+subplot(2,1,1) % First subplot in this figure window
+hold all % See Note 1 below
+xlabel("X position (m)")
+ylabel("Y position (m)")
+title("Sensitivity to launch angle phi")
+k= 0.02;       % friction coefficient, 0<=k<1
+dt= .05;      % size of time step
+v= 100;
+x= 0;
+y= 0;
+legendtext={};
+phiVec=linspace(0.1,pi/2,5);
+
+for i=1:length(phiVec)
+colr=[rand rand rand];
+legendText{i} = sprintf('phi=%.2f', phiVec(i)); %#ok<SAGROW> % % See Note 3 below
+
+[a,b]=golfTraj(dt,x,y,v,phiVec(i),k);
+   for j = 1:length(a)
+       pause(.01)
+       plot(a(j),b(j),"-o","Color",colr)
+   end
+end 
+legend(legendText) % Make a legend using the text in legendText
+
+
+% Add a title and labels for the x and y axes
+hold off % Needed before you do the next sensitivity analysis on k
+
+subplot(2,1,2)
+hold on
+
+xlabel("X position (m)")
+ylabel("Y position (m)")
+title("Sensitivity to Friction Coef k")
+
+dt= .05;      % size of time step
+phi=pi/4;
+v= 100;
+x= 0;
+y= 0;
+legendtext={};
+kVec=linspace(0.01,.2,5);
+
+for i=1:length(kVec)
+colr=[rand rand rand];
+legendText{i} = sprintf('k=%.2f', kVec(i)); %#ok<SAGROW> % % See Note 3 below
+
+[a,b]=golfTraj(dt,x,y,v,phi,kVec(i));
+   for j = 1:length(a)
+       pause(.01)
+       plot(a(j),b(j),"-o","Color",colr)
+   end
+end 
+legend(legendText) % Make a legend using the text in legendText
+
+
+hold off
