8c0f4aa9cb99ed83755848dd3ee5e081313329cc
hceisler <hceisler@gmail.com>
2022-10-02 23:54:05 -0400

    comments

--- Projects/pr_03/CornellTennis.m
+++ Projects/pr_03/CornellTennis.m
@@ -2,9 +2,10 @@ close all
 figure
 axis equal off
 hold on
-
+%sets parameters
 courtwidth=119.25;
 courtheight=75;
+%makes a three by four grid of tennis courts
 for x=0:courtwidth:courtwidth*3
     for y=0:courtheight:courtheight*2
         drawTennisCourt(x,y)
--- Projects/pr_03/drawTennisCourt.m
+++ Projects/pr_03/drawTennisCourt.m
@@ -1,11 +1,18 @@
 function drawTennisCourt(a,b)
+%Draws a tennis court as specified in the txb p5.3.7 where the bottom left
+%corner is at (a,b)
+
+%draws white background
 DrawRectNoBorder(0+a,0+b,79.25,35,"w")
+%Draws the top and bottom two rectangles
 DrawRectNoBorder(.25+a,.25+b,39.25,4.5, [41/256, 82/256, 39/256])
 DrawRectNoBorder(39.75+a,.25+b,39.25,4.5, [41/256, 82/256, 39/256])
 DrawRectNoBorder(39.75+a,30.25+b,39.25,4.5, [41/256, 82/256, 39/256])
 DrawRectNoBorder(.25+a,30.25+b,39.25,4.5, [41/256, 82/256, 39/256])
+%draws big two rectangles on the sides
 DrawRectNoBorder(.25+a,5+b,18,25, [41/256, 82/256, 39/256])
 DrawRectNoBorder(61+a,5+b,18,25, [41/256, 82/256, 39/256])
+%draws middle four rectangles
 DrawRectNoBorder(18.5+a,5+b,21,12.375, [41/256, 82/256, 39/256])
 DrawRectNoBorder(39.75+a,17.375+b,21,12.625, [41/256, 82/256, 39/256])
 DrawRectNoBorder(18.5+a,17.375+b,21,12.625, [41/256, 82/256, 39/256])
